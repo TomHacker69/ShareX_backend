4ba11675b3d6fadcc459324acdc2b7f9437066ad
TomHacker69 <ankitomar.shs@gmail.com>
2026-04-12 02:31:33 +0530

    Refactor requestRoutes.js for cleaner formatting

--- routes/requestRoutes.js
+++ routes/requestRoutes.js
@@ -1,18 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const Request = require("../models/Request");
-
-// Create request
 router.post("/borrow", async (req, res) => {
   const request = new Request(req.body);
   await request.save();
   res.json(request);
 });
-
-// Get requests for user
 router.get("/my", async (req, res) => {
   const requests = await Request.find({ borrowerId: req.query.userId });
   res.json(requests);
 });
-
 module.exports = router;
